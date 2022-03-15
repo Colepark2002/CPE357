c58ff7a4eaff61d41ef4364f6ae194674815217f
ColePark2002 <colepark2002@gmail.com>
2022-03-14 18:20:10 -0700

    Think I finished Even-Odd Sort

--- COLEPARK_Final/EvenOdd.c
+++ COLEPARK_Final/EvenOdd.c
@@ -6,63 +6,134 @@
 #include <sys/mman.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-#define INPUT 100
+#include <string.h>
+#include <time.h>
+#define INPUT 10
+
+
+
+void synch(int id, int par_count, int* ready)
+{
+    int newVal = ++ready[id];
+    for(int i = 0; i < par_count; i++)
+    {
+        while(ready[i] < newVal);
+    }
+    return;
+}
 
 int main(int argc, char* argv[])
 {
-    int size = INPUT;
-    int index;
-    char car;
-    int programs = atoi(argv[1]);
-    int *swaps = (int*)mmap(NULL,sizeof(int), PROT_WRITE | PROT_READ, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    int *Arr = (int*)mmap(NULL,sizeof(int) * size, PROT_WRITE | PROT_READ, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    int *Even = (int*)mmap(NULL,sizeof(int), PROT_WRITE | PROT_READ, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    int id;
-    int start;
-    int end;
-    int responsible;
+    int pid;
+    int mapsize = INPUT; // size of the mapped array
+    int ArrSize; // array size
+    char car; //used to find \n
+    int programs = atoi(argv[1]); // program count
+    int *swaps = (int*)mmap(NULL,sizeof(int), PROT_WRITE | PROT_READ, MAP_SHARED | MAP_ANONYMOUS, -1, 0); // Counts amount of swaps
+    int *Arr = (int*)mmap(NULL,sizeof(int) * mapsize, PROT_WRITE | PROT_READ, MAP_SHARED | MAP_ANONYMOUS, -1, 0); // Shared Array
+    int *Even = (int*)mmap(NULL,sizeof(int), PROT_WRITE | PROT_READ, MAP_SHARED | MAP_ANONYMOUS, -1, 0); // Might not use
+    int *ready = (int*)mmap(NULL, sizeof(int) * programs, PROT_WRITE | PROT_READ, MAP_SHARED | MAP_ANONYMOUS, -1,0); //used for synch
+    memset(ready, 0, sizeof(int) * programs);
+    int id; // process id
+    int start; // start index
+    int end; // end index
+    int responsible; // how many integers responsible for in array
     
 
     printf("\nEnter Input Array: ");
-    for(index = 0;; index++)
+    for(ArrSize = 0;; ArrSize++) // infinitely reads from input until hitting a \n
     {
-        if(index == size) // resize array if more than current size
+        if(ArrSize == mapsize) // resize array if more than current size
         {
-            size += INPUT;
-            Arr = (int*)mremap(Arr, index, size, 0);
+            mapsize += INPUT;
+            Arr = (int*)mremap(Arr, ArrSize, mapsize, 0);
         }
-        scanf("%d%c", &Arr[index], &car);
+        scanf("%d%c", &Arr[ArrSize], &car);
         
         if(car == '\n')
         {
             break;
         }
     }
-    index++;
-    for(int w = 0; w < index; w++)
+    ArrSize++;
+    if (programs > (ArrSize/2)) // if you have too many programs reset
+    {
+        programs = ArrSize/2;
+    }
+    printf("Initial Array: ");
+    for(int w = 0; w < ArrSize; w++) // prints array
     {
         printf("%d ", Arr[w]);
     }
-    return 0;
-    for(int x = 0; x < programs; x++)
+    printf("\n");
+    *swaps = 1;
+    for(int x = 0; x < programs; x++) // forks and initializes the children
     {
-        if(fork()==0)
+        if((pid = fork())==0)
         {
             id = x;
             if(id == 0)
             {
                 start = 0;
-                responsible = ((index/programs) + ((index) % programs));
+                responsible = ((ArrSize/programs) + ((ArrSize) % programs));
                 end = start + responsible;
+                break;
             }
             else
             {
-                responsible = ((index)/programs);
-                start = (id * responsible) + ((index) % programs);
+                responsible = ((ArrSize)/programs);
+                start = (id * responsible) + ((ArrSize) % programs);
                 end = start + responsible;
+                break;
             }
             
         }
     }
+    time_t starter = clock();
+    if (pid == 0)
+    {
+        while(*swaps != 0)
+        {
+            synch(id, programs, ready);
+            if(id == 0)
+                *swaps = 0;
+            for(int i = start; i < start + responsible && i < ArrSize - 1; i+=2)
+            {
+                if(Arr[i] > Arr[i+1])
+                {
+                    int temp = Arr[i];
+                    Arr[i] = Arr[i+1];
+                    Arr[i+1] = temp;
+                    *swaps += 1;
+                }
+            }
+            synch(id, programs, ready);
+            for(int i = start + 1; i < start + responsible && i < ArrSize - 1; i+=2)
+            {
+                if(Arr[i] > Arr[i+1])
+                {
+                    int temp = Arr[i];
+                    Arr[i] = Arr[i+1];
+                    Arr[i+1] = temp;
+                    *swaps += 1; 
+                }
+            }
+            
+        }
+    }
+    else
+    {
+        wait(NULL);
+        time_t ender = clock();
+        double time = ender - starter;
+        printf("Sorted Array: ");
+        for(int w = 0; w < ArrSize; w++) // prints array
+        {
+            printf("%d ", Arr[w]);
+        }
+        printf("\n\nProcesses: %d\nTime to Sort: %f secs\n", programs, time/CLOCKS_PER_SEC);
+    }
+    
+
     return 0;
 }
